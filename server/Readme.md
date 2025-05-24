59f09c83e537d8a948988c4bd4a6e6675d37da2a
jitunmohajan <jitunmohajan@gmail.com>
2025-05-24 18:51:03 +0600

    first commit

---
+++ server/Readme.md
@@ -0,0 +1,14 @@
+project setup
+npm init
+
+npm i dotenv
+
+npm i body-parser
+
+npm i express
+
+npm i mongoose
+
+npm install --save-dev nodemon
+
+npm i cors
---
+++ server/index.js
@@ -0,0 +1,26 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import route from "./routes/userRoutes.js";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+dotenv.config();
+
+const PORT = process.env.PORT || 7000;
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose
+  .connect(MONGOURL)
+  .then(() => {
+    console.log("DB connected successfully.");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port :${PORT} `);
+    });
+  })
+  .catch((error) => console.log(error));
+
+app.use("/api", route);
